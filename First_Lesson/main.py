0b7b0ec2fde7dd5e48ee736f5d97ab518f618262
Hamlet Poghosyan <hamlet.poghosyan@volo.global>
2021-03-27 16:53:04 +0400

    first lesson Basic Calculator and  input

--- First_Lesson/main.py
+++ First_Lesson/main.py
@@ -1,4 +1,6 @@
-#first lession
+from math import *
+
+#string lession
 
 character_name = "Jhon" #string
 character_age = 35     #integer
@@ -7,6 +9,8 @@ is_Male = True         #boolean
 #print(" my name is "+ character_name +",")
 #print(" im "+ character_age +"age old")
 #print("i love python ")
+
+"""  strings
 
 phease = "my first  lession"
 print(phease + " is cool")
@@ -23,3 +27,58 @@ print(phease[0])
 print(phease.index("l"))
 
 print(phease.replace("my","My"))
+
+"""
+
+#integer lession
+
+""" Integers
+my_num = 12
+print(2)
+print(2.1)
+print(-2.123)
+print(120+350)
+print(str(my_num)+" is my favorite number")
+print(abs(my_num))
+print(pow(4,6))
+print(max(4,6))
+print(min(4,6))
+print(round(4.9))
+print (floor(4.9))
+print (ceil(2.4))
+print (sqrt(36))
+
+"""
+
+# input
+
+""" 
+in Python 2.7  we use  raw_input()
+in Python 3 we use input()
+
+
+name = raw_input("Enter your Name : ")
+age = raw_input("Enter your age : ")
+
+print("Hello "+ name + "!"+ " You are " + age)
+
+"""
+
+# Basic Calculator
+
+"""
+"""
+
+num1 = input("Enter a number : ")
+num2 = input("Enter another number : ")
+
+result =int(num1)+int(num2)
+
+print(result)
+
+
+
+
+
+
+
