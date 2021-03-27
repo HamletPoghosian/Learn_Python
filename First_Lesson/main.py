7a4ff8e909abf8111f749d3897ac805c9ee19ff0
Hamlet Poghosyan <hamlet.poghosyan@volo.global>
2021-03-27 15:04:45 +0400

    first lesson working with strings in python

---
+++ First_Lesson/main.py
@@ -0,0 +1,25 @@
+#first lession
+
+character_name = "Jhon" #string
+character_age = 35     #integer
+is_Male = True         #boolean
+
+#print(" my name is "+ character_name +",")
+#print(" im "+ character_age +"age old")
+#print("i love python ")
+
+phease = "my first  lession"
+print(phease + " is cool")
+
+#strings function
+# upper()  and  isupper()
+
+phease = "my first  lession"
+
+print(len(phease))
+
+print(phease[0])
+
+print(phease.index("l"))
+
+print(phease.replace("my","My"))
