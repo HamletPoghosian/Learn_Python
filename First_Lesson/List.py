e0a1cc7bbed0160d4a890e99ecd991b684c9a500
Hamlet Poghosyan <hamlet.poghosyan@volo.global>
2021-03-28 13:23:18 +0400

    first lesson if statments-

--- First_Lesson/List.py
+++ First_Lesson/List.py
@@ -1,6 +1,6 @@
 print("Work with lists")
 
-friends = ["Jasmik","Jack","Bob"]
+friends = ["Jasmik", "Jack", "Bob"]
 
 print(friends)
 
@@ -12,58 +12,69 @@ print(friends[1:])  # get all start from 1 index
 
 print(friends[1:3])  # get all start from 1 index end of 3
 
-friends[0]= "Great Jasmik"
+friends[0] = "Great Jasmik"
 
 print(friends)
 
-
 # list Function --------------------------
 
-friends = ["Jasmik","Jack","Bob","Walter","Marry","Vivien"]
+friends = ["Jasmik", "Jack", "Bob", "Walter", "Marry", "Vivien"]
 
-lucky_numbers=[4,8,15,16,23,42]
+lucky_numbers = [4, 8, 15, 16, 23, 42]
 
 # extend add list to another list
 friends.extend(lucky_numbers)
 
-#append add only  | insert  add some index
+# append add only  | insert  add some index
 friends.append("Simona")
-friends.insert(5,"Gerald")
+friends.insert(5, "Gerald")
 
 # remove()-  only one | clear()- all
 
 friends.remove("Simona")
-#friends.clear()
+# friends.clear()
 lucky_numbers.sort()
 print(friends)
 
-#-------------------Tuples------------
+# -------------------Tuples------------
 
-coordinates = (4,5,4)
+coordinates = (4, 5, 4)
 print(coordinates)
 
-coordinates_list = [(4,5,4),(10,15,32),(7,8,9)]
+coordinates_list = [(4, 5, 4), (10, 15, 32), (7, 8, 9)]
 print(coordinates_list)
 
-#-----------------Functions---------
+
+# -----------------Functions---------
 
 def say_hi(name):
-     print("Hello User " +name)
+    print("Hello User " + name)
 
-#name=input("input your name: ")
-#say_hi(name)
+
+# name=input("input your name: ")
+# say_hi(name)
 
 
 def cube(num):
-    return num*num*num
+    return num * num * num
 
-result=cube(4)
+
+result = cube(4)
 print(result)
 
+# ----------if statments---
 
+is_male = False
+is_tall = False
 
-#----------if statments---
+if is_male or is_tall:
+    print("You are a male")
 
-"""
-"""
+elif is_male and not is_tall:
+    print("You are a not male")
 
+elif not is_male and not is_tall:
+    print("You are a not male 1")
+
+else:
+    print("Yur are not a male  2")
